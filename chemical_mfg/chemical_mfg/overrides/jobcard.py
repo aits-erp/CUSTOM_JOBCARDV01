a1566af2c5e30e4616a865d3a30725c2e4122bd1
saimore1234 <moresai899@gmail.com>
2026-01-23 12:36:56 +0530

    Adds

--- chemical_mfg/chemical_mfg/overrides/jobcard.py
+++ chemical_mfg/chemical_mfg/overrides/jobcard.py
@@ -1,13 +1,27 @@
 import frappe
-from frappe.utils import flt
 from erpnext.manufacturing.doctype.job_card.job_card import JobCard
 
 
 class CustomJobCard(JobCard):
 
-    # --------------------------------------------------
-    # DISABLE STANDARD VALIDATIONS (AS YOU WANT)
-    # --------------------------------------------------
+    # ❌ Disable timestamp conflict
+    def check_if_latest(self):
+        return
+
+    # ❌ Disable all validations
+    def validate(self):
+        return
+
+    def before_submit(self):
+        return
+
+    def on_submit(self):
+        return
+
+    def on_update(self):
+        return
+
+    # ❌ Kill internal logic
     def validate_job_card(self):
         return
 
@@ -17,7 +31,17 @@ class CustomJobCard(JobCard):
     def validate_job_card_qty(self):
         return
 
-    # --------------------------------------------------
-    # KEEP STANDARD VALIDATE + KILL PROCESS LOSS
-    # --------------------------------------------------
-    
+    def validate_time_logs(self):
+        return
+
+    def validate_process_loss(self):
+        return
+
+    def validate_operations(self):
+        return
+
+    def set_status(self):
+        return
+
+    def update_job_card_status(self):
+        return
