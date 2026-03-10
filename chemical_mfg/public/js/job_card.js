0a172e7d3c1691b27d31539b46f683ed54aee653
saimore1234 <moresai881@gmai.com>
2026-03-10 13:03:59 +0530

    Seuence

--- chemical_mfg/public/js/job_card.js
+++ chemical_mfg/public/js/job_card.js
@@ -1,14 +1,14 @@
 frappe.ui.form.on('Job Card', {
     refresh: function(frm) {
-
+        console.log("test");
         frm.page.wrapper.off('click', '[data-label="Complete%20Job"]');
-
+        
         frm.page.wrapper.on('click', '[data-label="Complete%20Job"]', function(e) {
-
+            
             if (!frm.doc.sequence_id || !frm.doc.work_order) {
                 return;
             }
-
+            
             frappe.call({
                 method: "frappe.client.get_list",
                 args: {
@@ -20,6 +20,7 @@ frappe.ui.form.on('Job Card', {
                     fields: ["name", "sequence_id", "status"]
                 },
                 callback: function(r) {
+                    console.log("test");
 
                     if (r.message && r.message.length) {
 
