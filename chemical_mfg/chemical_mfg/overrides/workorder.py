d66690f59782c4763338842d7d98ed94282ad855
saimore1234 <moresai881@gmai.com>
2026-05-12 18:59:09 +0530

    qc bypasss

--- chemical_mfg/chemical_mfg/overrides/workorder.py
+++ chemical_mfg/chemical_mfg/overrides/workorder.py
@@ -56,95 +56,6 @@
 #         else:
 #             self.manufactured_qty = flt(self.qty_completed)
 
-# import frappe
-# from frappe.utils import flt
-# from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder
-
-
-# class CustomWorkOrder(WorkOrder):
-
-    
-
-#     # --------------------------------------------------
-#     # DISABLE STRICT STANDARD VALIDATIONS
-#     # --------------------------------------------------
-#     def validate_completed_qty(self):
-#         pass
-
-#     def validate_qty_to_produce_against_completed_qty(self):
-#         pass
-
-#     def validate_qty_to_produce(self):
-#         pass
-
-#     # --------------------------------------------------
-#     # OPERATION STATUS (CHEMICAL LOGIC)
-#     # --------------------------------------------------
-#     def update_operation_status(self):
-#         for d in self.get("operations") or []:
-#             if flt(d.completed_qty) > 0:
-#                 d.status = "Completed"
-#             else:
-#                 d.status = "Pending"
-
-#     # --------------------------------------------------
-#     # REQUIRED ITEMS (DO NOT MERGE BY ITEM CODE)
-#     # --------------------------------------------------
-#     def set_required_items(self, reset_only_qty=False):
-#         """
-#         - Same item allowed in multiple operations
-#         - Qty scales correctly with Planned Qty
-#         - Safe for ERPNext v15
-#         """
-
-#         if reset_only_qty:
-#             return
-
-#         self.set("required_items", [])
-
-#         if not self.bom_no or not self.qty:
-#             return
-
-#         bom = frappe.get_doc("BOM", self.bom_no)
-#         bom_qty = flt(bom.quantity) or 1
-
-#         for item in bom.items:
-#             self.append("required_items", {
-#                 "item_code": item.item_code,
-#                 "item_name": item.item_name,
-#                 "description": item.description,
-#                 "operation": item.operation,
-#                 "required_qty": (flt(item.qty) / bom_qty) * flt(self.qty),
-#                 "uom": item.uom,
-#                 "stock_uom": item.stock_uom,
-#                 "conversion_factor": item.conversion_factor or 1,
-#                 "source_warehouse": item.source_warehouse or self.source_warehouse,
-#                 "allow_alternative_item": item.allow_alternative_item,
-#                 "include_item_in_manufacturing": 1,
-#                 "rate": item.rate,
-#                 "amount": flt(item.rate) * ((flt(item.qty) / bom_qty) * flt(self.qty)),
-#             })
-
-#     # --------------------------------------------------
-#     # MANUFACTURED QTY / PROCESS LOSS
-#     # --------------------------------------------------
-#     def update_work_order_qty(self):
-#         """
-#         - Kill process loss
-#         - Manufactured Qty = max completed operation qty
-#         """
-
-#         super().update_work_order_qty()
-
-#         self.process_loss_qty = 0
-
-#         if self.operations:
-#             self.manufactured_qty = max(
-#                 flt(op.completed_qty) for op in self.operations
-#             )
-#         else:
-#             self.manufactured_qty = flt(self.qty_completed)
-
 import frappe
 from frappe.utils import flt
 from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder
@@ -152,17 +63,7 @@ from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder
 
 class CustomWorkOrder(WorkOrder):
 
-    def validate(self):
-
-        try:
-            super().validate()
-
-        except Exception as e:
-
-            if "Could not find Quality Inspection" in str(e):
-                frappe.msgprint("Quality Inspection bypassed")
-            else:
-                raise
+    
 
     # --------------------------------------------------
     # DISABLE STRICT STANDARD VALIDATIONS
@@ -190,6 +91,11 @@ class CustomWorkOrder(WorkOrder):
     # REQUIRED ITEMS (DO NOT MERGE BY ITEM CODE)
     # --------------------------------------------------
     def set_required_items(self, reset_only_qty=False):
+        """
+        - Same item allowed in multiple operations
+        - Qty scales correctly with Planned Qty
+        - Safe for ERPNext v15
+        """
 
         if reset_only_qty:
             return
@@ -223,6 +129,10 @@ class CustomWorkOrder(WorkOrder):
     # MANUFACTURED QTY / PROCESS LOSS
     # --------------------------------------------------
     def update_work_order_qty(self):
+        """
+        - Kill process loss
+        - Manufactured Qty = max completed operation qty
+        """
 
         super().update_work_order_qty()
 
--- chemical_mfg/chemical_mfg/whitelisted/job_card_methods.py
+++ chemical_mfg/chemical_mfg/whitelisted/job_card_methods.py
@@ -1,24 +1,3 @@
-# import frappe
-
-# def apply_quality_template_from_work_order(doc, method=None):
-#     """
-#     Fetch custom_quality_template from Work Order Operation
-#     and set it in Job Card field quality_inspection_template
-#     """
-
-#     if not doc.work_order:
-#         return
-
-#     if not doc.operation:
-#         return
-
-#     wo = frappe.get_doc("Work Order", doc.work_order)
-
-#     for op in wo.operations:
-#         if op.operation == doc.operation:
-#             doc.quality_inspection_template = op.custom_quality_template
-#             break
-
 import frappe
 
 def apply_quality_template_from_work_order(doc, method=None):
@@ -37,11 +16,5 @@ def apply_quality_template_from_work_order(doc, method=None):
 
     for op in wo.operations:
         if op.operation == doc.operation:
-
-            if frappe.db.exists(
-                "Quality Inspection Template",
-                op.custom_quality_template
-            ):
-                doc.quality_inspection_template = op.custom_quality_template
-
+            doc.quality_inspection_template = op.custom_quality_template
             break
